becf4e375854b70b441f5cac9638dfbe6716e780
Shane Claussen <sclaussen@yahoo.com>
2020-04-01 12:10:36 -0700

    Added on/off debug messages to ledBuiltin sample

--- ledBuiltin/ledBuiltin.ino
+++ ledBuiltin/ledBuiltin.ino
@@ -1,11 +1,14 @@
 void setup() {
+    Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 
 void loop() {
+    Serial.println("off");
     digitalWrite(LED_BUILTIN, LOW);
     delay(200);
+    Serial.println("on");
     digitalWrite(LED_BUILTIN, HIGH);
     delay(200);
 }
